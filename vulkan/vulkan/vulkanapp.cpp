8a426e820c1f12916aecaef9ae26d119c5f1e638
Alex <tsoy92@list.ru>
2017-03-12 14:54:31 +0300

    add creating of surface and swapchain

--- vulkan/vulkan/vulkanapp.cpp
+++ vulkan/vulkan/vulkanapp.cpp
@@ -1,13 +1,31 @@
 #include "vulkanapp.h"
 #include <stdexcept>
 #include <vector>
+#include <iostream>		// for help
+
+void VulkanApp::initAppInfo()
+{
+	uint32_t extensionCount = 0;
+	auto extensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+
+	for (uint32_t i = 0; i < extensionCount; ++i)
+		info_.instanceExtensions.push_back(*(extensions + i));
+
+	// add desired extensions with info_.instanceExtensions.push_back(desiredExtension)
+}
 
 void VulkanApp::run()
 {
 	initWindow();
+	initAppInfo();		// rename function
+
 	createInstance();
 	pickPhysicalDevice();
 	createDevice();
+	createSurface();
+	createSwapchain();
+
+	showInfo();			// 
 
 	mainLoop();
 }
@@ -23,10 +41,13 @@ void VulkanApp::initWindow()
 	window_ = glfwCreateWindow(info_.WIDTH, info_.HEIGHT, info_.title, nullptr, nullptr);
 	if (!window_)
 		throw std::runtime_error("failed to create window");
+
 }
 
 void VulkanApp::createInstance()
 {
+	checkInstanceExtenstionSupport();
+
 	VkApplicationInfo appInfo = { };
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	appInfo.pApplicationName = info_.title;
@@ -39,7 +60,8 @@ void VulkanApp::createInstance()
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &appInfo;
 	createInfo.enabledLayerCount = 0;		// no layers
-	createInfo.enabledExtensionCount = 0;		// no extensions
+	createInfo.enabledExtensionCount = info_.instanceExtensions.size();
+	createInfo.ppEnabledExtensionNames = info_.instanceExtensions.data();
 
 	if (vkCreateInstance(&createInfo, nullptr, &instance_) != VK_SUCCESS)
 		throw std::runtime_error("failed to create instance");
@@ -55,6 +77,8 @@ void VulkanApp::pickPhysicalDevice()
 
 	std::vector<VkPhysicalDevice> physicalDevices(physicalDeviceCount);
 	vkEnumeratePhysicalDevices(instance_, &physicalDeviceCount, physicalDevices.data());
+
+	checkDeviceExtensionSupport(physicalDevices[0]);
 
 	physicalDevice_ = physicalDevices[0];		// now pick first physical device
 }
@@ -72,11 +96,44 @@ void VulkanApp::createDevice()
 	deviceCreateInfo.queueCreateInfoCount = 1;
 	deviceCreateInfo.pQueueCreateInfos = &deviceQueueCreateInfo;
 	deviceCreateInfo.enabledLayerCount = 0;			// no layers
-	deviceCreateInfo.enabledExtensionCount = 0;		// no extensions
+	deviceCreateInfo.enabledExtensionCount = info_.deviceExtensions.size();
+	deviceCreateInfo.ppEnabledExtensionNames = info_.deviceExtensions.data();
 	deviceCreateInfo.pEnabledFeatures = nullptr;
 
 	if (vkCreateDevice(physicalDevice_, &deviceCreateInfo, nullptr, &device_) != VK_SUCCESS)
 		throw std::runtime_error("failed to create logical device");
+}
+
+void VulkanApp::createSurface()
+{
+	if (glfwCreateWindowSurface(instance_, window_, nullptr, &surface_) != VK_SUCCESS)
+		throw std::runtime_error("failed to create window surface");
+}
+
+void VulkanApp::createSwapchain()
+{
+	// TODO: rewrite
+	VkSurfaceCapabilitiesKHR surfaceCapabilities = { };
+	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice_, surface_, &surfaceCapabilities);
+	
+ 	VkSwapchainCreateInfoKHR createInfo = { };
+	createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	createInfo.surface = surface_;
+	createInfo.minImageCount = surfaceCapabilities.minImageCount + 1;
+	createInfo.imageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+	createInfo.imageColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR;
+	createInfo.imageExtent = surfaceCapabilities.currentExtent;	
+	createInfo.imageArrayLayers = 1;						
+	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+	createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	createInfo.preTransform = surfaceCapabilities.currentTransform;		// no transform
+	createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	createInfo.presentMode = VK_PRESENT_MODE_MAILBOX_KHR;
+	createInfo.clipped = VK_TRUE;
+	createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+	if (vkCreateSwapchainKHR(device_, &createInfo, nullptr, &swapChain_) != VK_SUCCESS)
+		throw std::runtime_error("failed to create swap chain");
 }
 
 void VulkanApp::mainLoop()
@@ -84,7 +141,7 @@ void VulkanApp::mainLoop()
 	while (!glfwWindowShouldClose(window_)) 
 		glfwPollEvents();
 	
-	glfwDestroyWindow(window);
+	glfwDestroyWindow(window_);
 	glfwTerminate();
 }
 
@@ -111,9 +168,61 @@ uint32_t VulkanApp::getFamilyIndex()
 //	return -1;
 }
 
+void VulkanApp::checkInstanceExtenstionSupport()
+{
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+	for (const auto& ie : info_.instanceExtensions) {
+		for (auto beg = extensions.cbegin(); beg != extensions.cend(); ++beg) {
+			if (strcmp(beg->extensionName, ie) == 0)
+				break;
+
+			if (beg + 1 == extensions.cend()) {
+				std::string errorStr = "instance don't support " 
+					+ std::string(ie) + " extension";
+				throw std::runtime_error(errorStr);
+			}		
+		}
+	}
+}
+
+void VulkanApp::checkDeviceExtensionSupport(VkPhysicalDevice device)
+{
+	uint32_t extensionCount = 0;
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties>extensions(extensionCount);
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, extensions.data());
+
+	for (const auto& de : info_.deviceExtensions) {
+		for (auto beg = extensions.cbegin(); beg != extensions.cend(); ++beg) {
+			if (strcmp(beg->extensionName, de) == 0)
+				break;
+			
+			if (beg + 1 == extensions.cend()) {
+				std::string errorStr = "device don't support "
+					+ std::string(de) + " extension";
+				throw std::runtime_error(errorStr);
+			}
+		}
+	}
+}
+
 // delete functions
 void VulkanApp::cleanup()
 {
+	if (swapChain_) {
+		vkDestroySwapchainKHR(device_, swapChain_, nullptr);
+		swapChain_ = VK_NULL_HANDLE;
+	}
+
+	if (surface_) {
+		vkDestroySurfaceKHR(instance_, surface_, nullptr);
+		surface_ = VK_NULL_HANDLE;
+	}
+
 	if (device_) {
 		vkDestroyDevice(device_, nullptr);
 		device_ = VK_NULL_HANDLE;
@@ -128,4 +237,52 @@ void VulkanApp::cleanup()
 VulkanApp::~VulkanApp()
 {
 	cleanup();
+}
+
+
+// delete after relise
+void VulkanApp::showInfo()
+{
+	//--------------show instance layers-------------------------
+	uint32_t layerCount = 0;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+	std::vector<VkLayerProperties> layerProperties(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, layerProperties.data());
+
+	std::cout << "Instance layer names\n";
+	for (const auto& p : layerProperties)
+		std::cout << p.layerName << '\n';
+	std::cout << std::endl;
+
+	//--------------show instance extensions-----------------------
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties> extensions(extensionCount);	
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+		
+	std::cout << "Instance extension names\n";
+	for (const auto& ext : extensions)
+		std::cout << ext.extensionName << '\n';
+	std::cout << std::endl;
+
+	//--------------show device layers and properties---------------
+	if (physicalDevice_) {
+		vkEnumerateDeviceLayerProperties(physicalDevice_, &layerCount, nullptr);
+		layerProperties.resize(layerCount);
+		vkEnumerateDeviceLayerProperties(physicalDevice_, &layerCount, layerProperties.data());
+
+		std::cout << "Device layer names\n";
+		for (const auto& l : layerProperties)
+			std::cout << l.layerName << '\n';
+		std::cout << std::endl;
+
+		vkEnumerateDeviceExtensionProperties(physicalDevice_, nullptr, &extensionCount, nullptr);
+		extensions.resize(extensionCount);
+		vkEnumerateDeviceExtensionProperties(physicalDevice_, nullptr, &extensionCount, extensions.data());
+
+		std::cout << "Device extension names\n";
+		for (const auto& e : extensions)
+			std::cout << e.extensionName << '\n';
+		std::cout << std::endl;
+	}
 }
--- vulkan/vulkan/vulkanapp.h
+++ vulkan/vulkan/vulkanapp.h
@@ -2,29 +2,46 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW\glfw3.h>
+#include <vector>
 
 class VulkanApp {
 	GLFWwindow*	window_ = nullptr;
 	VkInstance instance_ = VK_NULL_HANDLE;
 	VkPhysicalDevice physicalDevice_ = VK_NULL_HANDLE;
 	VkDevice device_ = VK_NULL_HANDLE;
+	VkSurfaceKHR surface_ = VK_NULL_HANDLE;
+	VkSwapchainKHR swapChain_ = VK_NULL_HANDLE;
 
 	struct {					// struct for application info
 		int WIDTH = 800;
 		int HEIGHT = 600;
 		const char* title = "Vulkan application";
+
+		std::vector<const char*> instanceExtensions;
+
+		std::vector<const char*> deviceExtensions{
+			VK_KHR_SWAPCHAIN_EXTENSION_NAME
+		};
+
 	} info_;
 
 private:
+	void initAppInfo();
 	void initWindow();		// main functions
 	void createInstance();
 	void pickPhysicalDevice();
 	void createDevice();
+	void createSurface();
+	void createSwapchain();
 	void mainLoop();
 	void cleanup();
 
 private:		// help functions
 	uint32_t getFamilyIndex();
+	void checkInstanceExtenstionSupport();
+	void checkDeviceExtensionSupport(VkPhysicalDevice);
+
+	void showInfo();		// super help function for me, delete after relise
 
 public:
 	void run();
